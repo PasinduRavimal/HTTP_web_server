98066ce7836a3d80ff2fda4e53f8d7499ae386c5
Pasindu Ravimal <pasinduravimal@gmail.com>
2025-02-14 12:45:17 -0500

    Added error handling functions

---
+++ server/run_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -o server simple_server.c ../include/error_functions.c ../include/log.c
---
+++ server/simple_server.c
@@ -0,0 +1,11 @@
+#include "../include/common.h"
+#include "../include/log.h"
+
+int main() {
+    printf("Second run\n");
+    printf("Including Logs\n");
+
+    printf("Initializing Logs\n");
+    initializeLog();
+    
+}
