29d13c066409b97060944b34309ad29011e5d9ec
Pasindu Ravimal <pasinduravimal@gmail.com>
2025-02-18 13:23:48 -0500

    Added threading server

--- run_server.sh
+++ run_server.sh
@@ -9,8 +9,22 @@ if [ ! -f ./include/ename.c.inc ]; then
     ./include/Build_ename.sh > ./include/ename.c.inc
 fi
 
-echo "Compiling the web server..."
-gcc -o webserver ./server/simple_server.c ./include/error_functions.c ./include/log.c ./include/util.c ./include/core.c ./include/http.c ./include/inet_sockets.c ./include/ip.c
+echo "Select the server type:"
+echo "1) Threading Server"
+echo "2) Forking Server"
+read -p "Enter choice (1 or 2): " choice
+
+if [ "$choice" == "1" ]; then
+    SERVER_TYPE="threading_server"
+elif [ "$choice" == "2" ]; then
+    SERVER_TYPE="forking_server"
+else
+    echo "Invalid choice. Exiting."
+    exit 1
+fi
+
+echo "Compiling the $SERVER_TYPE..."
+gcc -o webserver ./server/${SERVER_TYPE}.c ./include/error_functions.c ./include/log.c ./include/util.c ./include/core.c ./include/http.c ./include/inet_sockets.c ./include/ip.c
 
 if [ $? -eq 0 ]; then
     echo "Compilation Successful. Running webserver..."
